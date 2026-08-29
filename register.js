483564fec0ffe00ec0afa6a293e334808a965781
Ahfande <157612444+Ahfande@users.noreply.github.com>
2026-08-29 23:22:30 +0700

    Update register.js

--- register.js
+++ register.js
@@ -1,5 +1,5 @@
 // register.js
-const API_URL = 'brew-co-production-f788.up.railway.app';
+const API_URL = 'https://brew-co-production-f788.up.railway.app/api';
 
 document.getElementById('registerForm').addEventListener('submit', async (e) => {
     e.preventDefault();
