50463d7b602ddc6af0ddfb372e92d6922eca50ef
Ahfande <157612444+Ahfande@users.noreply.github.com>
2026-08-30 16:32:37 +0700

    Update login-admin.js

--- login-admin.js
+++ login-admin.js
@@ -1,5 +1,5 @@
 // login-admin.js
-const API_URL = 'https://brew-co-production.up.railway.app/api';
+const API_URL = 'https://brew-co-production-56dd.up.railway.app/api';
 
 // Buat elemen notifikasi sukses
 const createSuccessMessage = (message) => {
